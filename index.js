f3b906060e93792ba9b3de1d257e6179e19271b6
Davoudnsr <davoud.nasrabadi@gmail.com>
2020-12-18 20:45:20 +0330

    server initial config done

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const config = require('./config');
+const morgan = require('morgan');
+const path = require('path');
+const cors = require('cors');
+const bodyParser= require('body-parser');
 const app = express();
-
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
 app.get('/',(req,res)=>{
-   res.json(200).send('Ok');
+   res.status(200).json({message:'Ok'});
 })
-
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/dist'));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','dist','index.html'));
+    })
+}
 app.listen(config.PORT,()=>console.log('Server is running on port '+config.PORT));
